117ce722e84a05e1c84e78c27cb8f9acaee214c3
Romantic <m15115021148@163.com>
2018-11-08 12:17:48 +0800

    fix c class way

--- jni/cmdutils.c
+++ jni/cmdutils.c
@@ -200,15 +200,15 @@ void show_help_options(const OptionDef *options, const char *msg, int req_flags,
     printf("\n");
 }
 
-void show_help_children(const AVClass *class, int flags)
+void show_help_children(const AVClass *clz, int flags)
 {
     const AVClass *child = NULL;
-    if (class->option) {
-        av_opt_show2(&class, NULL, flags, 0);
+    if (clz->option) {
+        av_opt_show2(&clz, NULL, flags, 0);
         printf("\n");
     }
 
-    while (child = av_opt_child_class_next(class, child))
+    while (child = av_opt_child_class_next(clz, child))
         show_help_children(child, flags);
 }
 
